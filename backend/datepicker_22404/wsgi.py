a0e3b20342532dfe589e6f125de85b04c78b60de
Crowdbotics <team@crowdbotics.com>
2020-11-08 17:18:39 +0000

    Initial commit.

---
+++ backend/datepicker_22404/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for datepicker_22404 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'datepicker_22404.settings')
+
+application = get_wsgi_application()
